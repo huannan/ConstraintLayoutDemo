09ce5ab4e8993f6038c8c432320dd53c9aa29b46
吴焕楠 <wuhuannan@meizu.com>
2020-05-08 19:13:16 +0800

    first commit

---
+++ app/src/main/java/com/nan/constraintlayoutdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nan.constraintlayoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * https://blog.csdn.net/guolin_blog/article/details/53122387
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
